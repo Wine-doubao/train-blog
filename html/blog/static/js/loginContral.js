f05e123cc668754f41e994d41d406f92493db73c
doubao <2156270642@qq.com>
2021-07-22 10:26:58 +0800

    nginx first commit

---
+++ html/blog/static/js/loginContral.js
@@ -0,0 +1,40 @@
+loginContral();
+function loginContral() {
+    var username = $.cookie("username");
+    var path = window.location.pathname;
+    var mana = path.split("/");
+    if(!!username) {
+        if(mana[1] == "index.html" || mana[1] == "") logined(true, username);
+    }else{
+        if(mana[1] == "index.html" || mana[1] == "") logined();
+
+        // 未登录不能进入个人中心页面
+        if (mana.length == 3 && (mana[2] == "manage.html" || mana[2].indexOf("article.html") == 0)) {
+            window.location.replace('/')
+        }
+    }
+}
+
+// 渲染登录和未登录的导航栏
+function logined(logined = false,username) {
+    var navLoginSign = $(".nav-login-sign")[0];
+    var renderLoginSign = template("navLoginSign",{
+        logined,
+        username
+    });
+    console.log(logined);
+    console.log(username);
+    navLoginSign.innerHTML = renderLoginSign;
+    // 添加退出登录到cancel
+    if(logined) {
+        $(".out-login").click(function() {
+            outLogin();
+        })
+    }
+}
+
+// 退出登录
+function outLogin(){
+    $.removeCookie("username");
+    $.removeCookie("password");
+}
